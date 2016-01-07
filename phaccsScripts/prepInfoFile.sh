421b91358cf4377af58cfa085e8b4ebf2e14c8ac
Thea Van Rossum <thea.van.rossum@gmail.com>
2016-01-06 21:42:46 -0800

    don't run phaccs on empty (failed) circonspect results

--- phaccsScripts/prepInfoFile.sh
+++ phaccsScripts/prepInfoFile.sh
@@ -1,3 +1,3 @@
-ls /home/tva4/watershedMetagenomics/analysis/viral/VcDNA/circonspect/defaults/*/circonspect.csp > col2
+find /home/tva4/watershedMetagenomics/analysis/viral/VcDNA/circonspect/defaults/*/circonspect.csp -type f -size +0 > col2
 cat col2 | cut -d "_" -f 2 > col1
 paste col1 col2 > infoForQsubRuns.txt
